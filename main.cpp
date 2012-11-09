a1a6e3bfb0544c2089e39c10fca7291279bd9d9c
Ankush@Fraunhofer <Ankush@Fraunhofer>
2012-11-09 11:45:56 +0100

    ideal coding

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <list>
+
+using namespace std;
 
 
-//editing from realitymaps :)
-//editing from room :)
-int main(int argc, char **argv) {
-    std::cout << "Hello, world! :))" << std::endl;
-    return 0;
+//structure for edge
+struct EDGE
+{
+	unsigned int capacity;
+	unsigned int flow;
+};
+
+//structure for a vertex
+struct NODE
+{
+	unsigned int id;
+	bool isBlocked;
+	int fin;
+	int fout;
+	list<EDGE*> incoming_edges;
+	list<EDGE*> outgoing_edges;
+};  
+
+int main(int argc, char **argv) 
+{
+	std::cout << "Hello, world! :))" << std::endl;
+	return 0;
 }
