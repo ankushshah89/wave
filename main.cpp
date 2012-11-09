680a92aacebf2866d37692e899c7752fe4226935
Ankush@Fraunhofer <Ankush@Fraunhofer>
2012-11-09 13:53:05 +0100

    coding complete

--- main.cpp
+++ main.cpp
@@ -4,11 +4,18 @@
 using namespace std;
 
 #define MAXIMUM_NODES 100
+
+
 int no_of_vertices;
 int capacity_matrix[MAXIMUM_NODES][MAXIMUM_NODES];
 int flow_matrix[MAXIMUM_NODES][MAXIMUM_NODES];
 int vertices[MAXIMUM_NODES][3];
 	
+void initialise();
+int incoming_flow(int);
+int outgoing_flow(int);
+void print();
+void start_wave();
 
 void initialise()
 {
@@ -44,8 +51,86 @@ void print()
 		}
 		cout<<endl;
 	}
-	
 }
+
+
+void start_wave()
+{
+	for(int i=0;i<no_of_vertices;i++)
+	{
+		flow_matrix[0][i] = capacity_matrix[0][i];
+	}
+}
+
+void increase_flow()
+{
+	bool balanced;
+	int add_flow;
+	for(int i=1;i<no_of_vertices-1;i++)
+	{
+		balanced = true;
+		if(vertices[i][0] == 1 && incoming_flow(i) != outgoing_flow(i) )
+		{
+			balanced =false;
+			//attempt to balance the node
+			for(int j=0;j<no_of_vertices && vertices[j][0] == 1;j++)
+			{
+				add_flow = min(capacity_matrix[i][j]-flow_matrix[i][j],incoming_flow(i) - outgoing_flow(i));
+				flow_matrix[i][j] = flow_matrix[i][j] + add_flow;
+				
+				if(incoming_flow(i) != outgoing_flow(i))
+				{
+					balanced = true;
+					break;
+				}
+			}
+		}
+		
+		//if unable to balance the vertex
+		if(balanced == false)
+		{
+			vertices[i][0] = 0;
+		}
+	}
+}
+
+int incoming_flow(int vertex_id)
+{
+	int fin = 0;
+	for(int i=0;i<no_of_vertices;i++)
+	{
+		fin = fin + flow_matrix[i][vertex_id];
+	}
+	return fin;
+}
+
+int outgoing_flow(int vertex_id)
+{
+	int fout = 0;
+	for(int i=0;i<no_of_vertices;i++)
+	{
+		fout = fout + flow_matrix[vertex_id][i];
+	}
+	return fout;
+}
+	
+
+void decrease_flow()
+{
+	int subtract_flow;
+	for(int j=no_of_vertices-2;j>0;j--)
+	{
+		if(vertices[j][0] == 0 && incoming_flow(j) != outgoing_flow(j) )
+		{
+			for(int i=0;i<no_of_vertices;i++)
+			{
+				subtract_flow = min(flow_matrix[i][j],incoming_flow(j) - outgoing_flow(j));
+				flow_matrix[i][j] = flow_matrix[i][j] - subtract_flow;
+			}	
+		}
+	}	
+}
+
 
 int main(int argc, char **argv) 
 {
@@ -60,6 +145,8 @@ int main(int argc, char **argv)
 		capacity_matrix[v][w] = capacity;
 	}
 	
+	start_wave();
+	increase_flow();
+	decrease_flow();
 	print();
-	
 }
